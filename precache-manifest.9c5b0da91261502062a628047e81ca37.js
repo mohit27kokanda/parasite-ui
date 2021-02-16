44196e00c0b033724f3742028d76aafa54873aea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-16 07:12:42 +0000

    Deploy parasite.todack.com to github.com/todack/parasite-ui.git:release

---
+++ precache-manifest.9c5b0da91261502062a628047e81ca37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2ea0d70777308692a6",
+    "url": "/css/chunk-vendors.41f0a7ec.css"
+  },
+  {
+    "revision": "ec5332310a180ebab5f7f145c2654508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2841156544d11a1ed46c",
+    "url": "/js/app.6ff786fa.js"
+  },
+  {
+    "revision": "8f2ea0d70777308692a6",
+    "url": "/js/chunk-vendors.4f0cd8bd.js"
+  },
+  {
+    "revision": "b16d5eba6e0bfab28d1976343015197c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
